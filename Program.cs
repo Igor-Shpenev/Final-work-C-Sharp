151a7a537edc50ee34df1154ef14702dfb29abc3
Igor Shpenev <vdvshneek@yandex.ru>
2022-12-02 17:44:57 +0500

    I wrote a method for displaying a new array on the screen

--- Program.cs
+++ Program.cs
@@ -12,3 +12,10 @@ string[] CreatNewArray(string[] array)
     }
     return newarray;
 }
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
